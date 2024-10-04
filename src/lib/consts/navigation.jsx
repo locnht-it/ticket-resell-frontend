36c14dcbbe5cebdc72b0a01df07e9f49e6a279ce
locnht-it <locnht.it@gmail.com>
2024-10-04 16:29:54 +0700

    create layout

---
+++ src/lib/consts/navigation.jsx
@@ -0,0 +1,70 @@
+import {
+  HiOutlineViewGrid,
+  HiOutlineCube,
+  HiOutlineShoppingCart,
+  HiOutlineUsers,
+  HiOutlineQuestionMarkCircle,
+  HiOutlineAnnotation,
+  HiOutlineTicket,
+  HiOutlineCurrencyDollar,
+} from "react-icons/hi";
+
+export const DASHBOARD_SIDEBAR_LINKS = [
+  {
+    key: `dashboard`,
+    label: `Dashboard`,
+    path: `/`,
+    icon: <HiOutlineViewGrid />,
+  },
+  {
+    key: `transaction`,
+    label: `Transaction`,
+    path: `/transaction`,
+    icon: <HiOutlineCurrencyDollar />,
+  },
+  {
+    key: `platformFee`,
+    label: `Platform Fee`,
+    path: `/platform-fee`,
+    icon: <HiOutlineCube />,
+  },
+  {
+    key: `user`,
+    label: `User`,
+    path: `/user`,
+    icon: <HiOutlineUsers />,
+  },
+  {
+    key: `post`,
+    label: `Post`,
+    path: `/post`,
+    icon: <HiOutlineAnnotation />,
+  },
+  {
+    key: `ticket`,
+    label: `Ticket`,
+    path: `/ticket`,
+    icon: <HiOutlineTicket />,
+  },
+  {
+    key: `order`,
+    label: `Order`,
+    path: `/order`,
+    icon: <HiOutlineShoppingCart />,
+  },
+];
+
+export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
+  // {
+  //   key: `settings`,
+  //   label: `Settings`,
+  //   path: `/settings`,
+  //   icon: <HiOutlineCog />,
+  // },
+  {
+    key: `support`,
+    label: `Help & Support`,
+    path: `/support`,
+    icon: <HiOutlineQuestionMarkCircle />,
+  },
+];
